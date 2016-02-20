62622bf57feb66634d36c771f49c41a96b610a2b
Darshil Chanpura <dtchanpura@gmail.com>
2016-02-20 21:04:37 +0530

    Update: Constants file added

---
+++ flaskapp/constants.py
@@ -0,0 +1,8 @@
+from flaskapp import app
+
+
+class RedisConstants:
+    REDIS_HOST = app.config.get('REDIS_HOST')
+    REDIS_PORT = app.config.get('REDIS_PORT')
+    REDIS_DB = app.config.get('REDIS_DB')
+    REDIS_SESSION_COUNTER = "total_sessions"
