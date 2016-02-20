273405a55f0cc4a1b12c3820bfd549cc972a6d44
Darshil Chanpura <dtchanpura@gmail.com>
2016-02-20 22:14:49 +0530

    Session updated and returning appropriate codes

--- flaskapp/functions.py
+++ flaskapp/functions.py
@@ -1,3 +1,4 @@
+import json
 from uuid import uuid4
 
 import redis
@@ -8,23 +9,37 @@ from flaskapp.constants import RedisConstants
 def generate_random_session_id():
     """
     Generate a random string for unique session id.
-    TODO Add check in database
 
     :return: Returns a unique uuid.
     """
-    return str(uuid4())
+    session_id = str(uuid4())
+    r = _get_redis()
+    if r.get(session_id) is None:
+        return session_id
+    else:
+        generate_random_session_id()
 
 
 def update_or_create_session(session_id, data=None):
-    r = get_redis()
-    r.set(session_id, data)
+    r = _get_redis()
+    r.set(session_id, json.dumps(data))
     r.incr(RedisConstants.REDIS_SESSION_COUNTER)
 
 
+def is_active_session(session_id):
+    r = _get_redis()
+    return r.get(session_id) is not None
+
+
 def get_session_data(session_id):
-    r = get_redis()
-    return r.get(session_id)
+    r = _get_redis()
+    data = r.get(session_id)
+    if data is None:
+        return data
+    return data.decode()
 
 
-def get_redis():
-    return redis.StrictRedis(host=RedisConstants.REDIS_HOST, port=RedisConstants.REDIS_PORT, db=RedisConstants.REDIS_DB)
+def _get_redis():
+    return redis.StrictRedis(host=RedisConstants.REDIS_HOST,
+                             port=RedisConstants.REDIS_PORT,
+                             db=RedisConstants.REDIS_DB)
--- flaskapp/views.py
+++ flaskapp/views.py
@@ -1,9 +1,7 @@
-import json
-
 from flask import make_response, request
 
 from flaskapp import app
-from flaskapp.functions import generate_random_session_id, update_or_create_session, get_session_data
+from flaskapp.functions import *
 
 
 @app.route('/version')
@@ -22,9 +20,11 @@ def version():
 @app.route('/session/<session_id>', methods=['GET', 'POST', 'PUT'])
 def get_session(session_id):
     """
-    GET Method will generate a new session id
-    POST Method requires a json with session_id in it.
+    GET Method will generate a new session id or get details about it.
+    POST Method requires a json with data in it.
+    PUT Method also requires json with data in it.
 
+    :param session_id: Session ID for particular session
     :return: Responds with session_id and data object
     """
     response_dict = {}
@@ -33,21 +33,36 @@ def get_session(session_id):
         # with or without json data
         # session_id = request.json.get('session_id')
         if not session_id:
-            return return_response({"message": "Something is missing, read the API docs for more information."}, 403)
+            return return_response({"message": "Something is missing, "
+                                               "read the API docs for "
+                                               "more information."}, 403)
+        if is_active_session(session_id):
+            return return_response({"message": "Conflict, ID already exists. Use PUT instead of POST."}, 409)
         if request.json:
             update_or_create_session(session_id=session_id, data=request.json.get('data'))
-            response_dict = {"ok": True}
+            response_dict['ok'] = True
+    elif request.method == 'PUT' and request.json:
+        # Updating information in session
+        if not session_id:
+            return return_response({"message": "Something is missing, "
+                                               "read the API docs for "
+                                               "more information."}, 403)
+        if request.json:
+            update_or_create_session(session_id=session_id, data=request.json.get('data'))
+            response_dict['ok'] = True
     elif request.method == 'GET':
-        # Getting information for a session_id
+        # Getting information for a session_id or get new random session_id
         if session_id is None:
             response_dict['session_id'] = generate_random_session_id()
         else:
-            response_dict['data'] = get_session_data(session_id=session_id)
-            response_dict['ok'] = True
+            data = get_session_data(session_id=session_id)
+            if data is not None:
+                response_dict = {'data': data, 'ok': True}
+            else:
+                return return_response({"message": "ID does not exists"}, 404)
     else:
-        # PUT method to update the given data
-        update_or_create_session(session_id=session_id, data=request.json.get('data'))
-        response_dict['ok'] = True
+        pass
+
     return return_response(response_dict)
 
 
