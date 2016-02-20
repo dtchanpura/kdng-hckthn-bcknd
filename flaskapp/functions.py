4f91b2b50193fe539afe1c368e19c0e541827df4
Darshil Chanpura <dtchanpura@gmail.com>
2016-02-20 21:22:22 +0530

    Update: Session method created for updating and getting session details

--- flaskapp/functions.py
+++ flaskapp/functions.py
@@ -1,11 +1,30 @@
 from uuid import uuid4
 
+import redis
+
+from flaskapp.constants import RedisConstants
+
 
 def generate_random_session_id():
     """
     Generate a random string for unique session id.
     TODO Add check in database
 
-    :return:
+    :return: Returns a unique uuid.
     """
     return str(uuid4())
+
+
+def update_or_create_session(session_id, data=None):
+    r = get_redis()
+    r.set(session_id, data)
+    r.incr(RedisConstants.REDIS_SESSION_COUNTER)
+
+
+def get_session_data(session_id):
+    r = get_redis()
+    return r.get(session_id)
+
+
+def get_redis():
+    return redis.StrictRedis(host=RedisConstants.REDIS_HOST, port=RedisConstants.REDIS_PORT, db=RedisConstants.REDIS_DB)
--- flaskapp/views.py
+++ flaskapp/views.py
@@ -3,27 +3,62 @@ import json
 from flask import make_response, request
 
 from flaskapp import app
-from flaskapp.functions import generate_random_session_id
+from flaskapp.functions import generate_random_session_id, update_or_create_session, get_session_data
 
 
 @app.route('/version')
 def version():
+    """
+    Get the version of API being deployed
+
+    :return: Responds with a simple json {"version": "0.0.1b"}
+    """
     response = make_response('{"version" : %s }' % app.config.get('VERSION'), 200)
     response.content_type = "application/json"
     return response
 
 
-@app.route('/session', methods=['GET', 'POST'])
-def get_new_session():
-    response_dict = {}
-    if request.method == 'GET':
-        response_dict['request_id'] = generate_random_session_id()
-        response_dict['data'] = {}
-    elif request.method == 'POST':
-        response_dict = {}
-    else:
-        response_dict = {}
+@app.route('/session/', methods=['GET', 'POST', 'PUT'], defaults={'session_id': None})
+@app.route('/session/<session_id>', methods=['GET', 'POST', 'PUT'])
+def get_session(session_id):
+    """
+    GET Method will generate a new session id
+    POST Method requires a json with session_id in it.
 
+    :return: Responds with session_id and data object
+    """
+    response_dict = {}
+    if request.method == 'POST' and request.json:
+        # First Time creation
+        # with or without json data
+        # session_id = request.json.get('session_id')
+        if not session_id:
+            return return_response({"message": "Something is missing, read the API docs for more information."}, 403)
+        if request.json:
+            update_or_create_session(session_id=session_id, data=request.json.get('data'))
+            response_dict = {"ok": True}
+    elif request.method == 'GET':
+        # Getting information for a session_id
+        if session_id is None:
+            response_dict['session_id'] = generate_random_session_id()
+        else:
+            response_dict['data'] = get_session_data(session_id=session_id)
+            response_dict['ok'] = True
+    else:
+        # PUT method to update the given data
+        update_or_create_session(session_id=session_id, data=request.json.get('data'))
+        response_dict['ok'] = True
+    return return_response(response_dict)
+
+
+def return_response(response_dict, code=200):
+    """
+    Utility function for returning a decorated response
+
+    :param response_dict: dictionary to be returned as json
+    :param code: status code to be returned
+    :return: Response object for flask's request
+    """
     response = make_response(json.dumps(response_dict))
     response.content_type = "application/json"
-    return response
+    return response, code
